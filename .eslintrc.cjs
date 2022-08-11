419a81c12d9330f8ac9368d12dcb79d103a3b6c1
mullore <928358747@qq.com>
2022-08-12 02:14:50 +0800

    refactor: 配置three环境

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,31 +1,27 @@
 module.exports = {
   env: {
     browser: true,
-    es2021: true,
+    es2021: true
     // node: true
   },
   extends: [
     'plugin:vue/vue3-essential',
     'standard',
-     // 1. 接入 prettier 的规则
-     "prettier",
-     "plugin:prettier/recommended"
+    // 1. 接入 prettier 的规则
+    'prettier',
+    'plugin:prettier/recommended'
   ],
   parserOptions: {
     ecmaVersion: 'latest',
     parser: '@typescript-eslint/parser',
     sourceType: 'module'
   },
-  plugins: [
-    'vue',
-    '@typescript-eslint',
-    "prettier"
-  ],
+  plugins: ['vue', '@typescript-eslint', 'prettier'],
   rules: {
     // 3. 注意要加上这一句，开启 prettier 自动修复的功能
-    "prettier/prettier": "error",
-    quotes: ["error", "single"],
-    semi: ["error", "always"],
-    "react/react-in-jsx-scope": "off"
+    'prettier/prettier': 'error',
+    quotes: ['error', 'single'],
+    semi: ['error', 'always'],
+    'react/react-in-jsx-scope': 'off'
   }
-}
+};
--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,17 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
+import glsl from 'vite-plugin-glsl';
 // vite.config.ts
 import viteEslint from 'vite-plugin-eslint';
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), viteEslint()]
+  plugins: [vue(), viteEslint(), glsl()],
+  resolve: {
+    // 别名配置
+    alias: {
+      '@assets': path.join(__dirname, 'src/assets')
+    }
+  }
 });
