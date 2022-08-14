14de438cfc1ea099e4839344e037a787e527b675
mullore <928358747@qq.com>
2022-08-15 00:30:37 +0800

    chore: 增设补充Eslint+perttier配置

--- vite.config.ts
+++ vite.config.ts
@@ -3,15 +3,23 @@ import vue from '@vitejs/plugin-vue';
 import glsl from 'vite-plugin-glsl';
 // vite.config.ts
 import viteEslint from 'vite-plugin-eslint';
+import vueJsx from '@vitejs/plugin-vue-jsx';
 import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), viteEslint(), glsl()],
+  plugins: [vue(), viteEslint(), glsl(), vueJsx()],
+
   resolve: {
+    extensions: ['.ts', '.mjs', '.js', '.jsx', '.tsx', '.json', '.vue'],
     // 别名配置
     alias: {
+      '@': path.join(__dirname, 'src'),
       '@assets': path.join(__dirname, 'src/assets'),
+      '@comps': path.join(__dirname, 'src/components'),
+      '@utils': path.join(__dirname, 'src/utils'),
+      '@router': path.join(__dirname, 'src/router'),
+      '@store': path.join(__dirname, 'src/store'),
     },
   },
 });
