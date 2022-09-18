f1e02cc051b8381790756c02bbca02a59ac1d21d
mullore <928358747@qq.com>
2022-09-18 12:15:31 +0800

    chore: 添加微应用

---
+++ examples/three/src/main.ts
@@ -0,0 +1,41 @@
+import { App,createApp } from 'vue';
+import './style.css';
+import app from './App.vue';
+import { renderWithQiankun, qiankunWindow } from '../../../es/helper';
+// createApp(App).mount('#app')
+let root: App;
+
+function render(props: any) {
+  const { container } = props;
+  root = createApp(app);
+  const c = container
+    ? container.querySelector('#app')
+    : document.getElementById('app');
+  root.mount(c);
+}
+
+console.log(3333);
+
+renderWithQiankun({
+  mount(props) {
+    console.log('vue3sub mount');
+    render(props);
+  },
+  bootstrap() {
+    console.log('bootstrap');
+  },
+  unmount(props: any) {
+    console.log('vue3sub unmount');
+    root.unmount();
+  },
+  update(props: any) {
+    console.log('vue3sub update');
+    console.log(props);
+  },
+});
+
+if (!qiankunWindow.__POWERED_BY_QIANKUN__) {
+  render({});
+}
+
+// createApp(App).mount('#app')
